0258bcaa0410876f65fba58d97032adee071c835
Matteo Papa <papa.1896945@studenti.uniroma1.it>
2025-08-22 11:31:35 +0200

    Some code cleaning

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from bin.bin_simulator import BinSimulator
 from mqtt.central_station import CentralStation
 
 if __name__ == "__main__":
-    # Avvia simulatore cassonetti in un thread separato
     sim_thread = threading.Thread(target=lambda: BinSimulator().run())
     sim_thread.start()
 
-    # Avvia la stazione centrale (bloccante)
     CentralStation().run()
